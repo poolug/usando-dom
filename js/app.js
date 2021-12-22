3d8622141b53f815ccd3026e22f1f43f5e01f39f
poolug <pauluzcategui43@gmail.com>
2021-12-21 22:07:12 -0300

    Avances row-col8 (Billing address)

--- js/app.js
+++ js/app.js
@@ -178,7 +178,7 @@ function rowCol4() {
     
     let Spanli4 = document.createElement('span');
     Spanli4.className = 'text-success';
-    let Spanli4Text = document.createTextNode('$-5');
+    let Spanli4Text = document.createTextNode('-$5');
     Spanli4.appendChild(Spanli4Text);
 
     Divli4.appendChild(Divli4H6) && Divli4.appendChild(Small1);
@@ -194,7 +194,7 @@ function rowCol4() {
     ul.appendChild(li5);
     
     let SpanLi5 = document.createElement('span');
-    // SpanLi5.className = 'text-muted';
+    SpanLi5.className = 'text-muted';
     let SpanLi5Text = document.createTextNode('Total (USD)');
     SpanLi5.appendChild(SpanLi5Text);
 
@@ -243,6 +243,117 @@ function rowCol4() {
 function rowCol8() {
   let div = document.createElement('div');
   div.className = 'col-md-8 order-md-1';
+
+  let divH4 = document.createElement('h4');
+  divH4.className = 'mb-3';
+  let divH4Text = document.createTextNode('Billing address');
+  divH4.appendChild(divH4Text);
+
+  div.appendChild(divH4);
+
+  function form(){
+    let form = document.createElement('form');
+    form.className = 'needs-validation';
+    form.setAttribute('novalidate','');
+
+    function row1Form(){
+      let row = document.createElement('div');
+      row.className = 'row';
+
+      function divRowCol6a() {
+        let div1Row = document.createElement('div');
+        div1Row.className = 'col-md-6 mb-3';
+
+        let labelDiv1Row = document.createElement('label');
+        labelDiv1Row.setAttribute('for','firstName');
+        let labelDiv1RowText = document.createTextNode('First name');
+        labelDiv1Row.appendChild(labelDiv1RowText);
+
+        let inputDiv1Row = document.createElement('input');
+        inputDiv1Row.setAttribute('type','text');
+        inputDiv1Row.className = 'form-control';
+        inputDiv1Row.setAttribute('id','firstName') + inputDiv1Row.setAttribute('placeholder','');
+        inputDiv1Row.setAttribute('value','') + inputDiv1Row.setAttribute('required','');
+        
+        let div = document.createElement('div');
+        div.className = 'invalid-feedback';
+        let divText = document.createTextNode('Valid first name is required. ');
+        div.appendChild(divText);
+        
+        div1Row.appendChild(labelDiv1Row) && div1Row.appendChild(inputDiv1Row) && div1Row.appendChild(div);
+
+        return div1Row
+      }
+
+      function divRowCol6b() {
+        let div1Row = document.createElement('div');
+        div1Row.className = 'col-md-6 mb-3';
+
+        let labelDiv1Row = document.createElement('label');
+        labelDiv1Row.setAttribute('for','lastName');
+        let labelDiv1RowText = document.createTextNode('Last name');
+        labelDiv1Row.appendChild(labelDiv1RowText);
+
+        let inputDiv1Row = document.createElement('input');
+        inputDiv1Row.setAttribute('type','text');
+        inputDiv1Row.className = 'form-control';
+        inputDiv1Row.setAttribute('id','lastName') + inputDiv1Row.setAttribute('placeholder','');
+        inputDiv1Row.setAttribute('value','') + inputDiv1Row.setAttribute('required','');
+        
+        let div = document.createElement('div');
+        div.className = 'invalid-feedback';
+        let divText = document.createTextNode('Valid last name is required. ');
+        div.appendChild(divText);
+        
+        div1Row.appendChild(labelDiv1Row) && div1Row.appendChild(inputDiv1Row) && div1Row.appendChild(div);
+
+        return div1Row
+      }
+      row.appendChild(divRowCol6a()) && row.appendChild(divRowCol6b());
+
+      return row;
+    }
+    
+    function divsForm(){
+      let div1 = document.createElement('div');
+      div1.className = 'mb-3';
+      let label = document.createElement('label');
+      label.setAttribute('for','username')
+      let labelText = document.createTextNode('Username')
+      label.appendChild(labelText) && div1.appendChild(label);
+
+      let div1a = document.createElement('div');
+      div1a.className = 'input-group';
+      let div1ab = document.createElement('div');
+      div1ab.className = 'input-group-preprend';
+      div1a.appendChild(div1ab);
+      let spanDiv1ab = document.createElement('span');
+      spanDiv1ab.className = 'input-group-text';
+      let spanDiv1abText = document.createTextNode('@');
+      spanDiv1ab.appendChild(spanDiv1abText);
+      div1ab.appendChild(spanDiv1ab);
+      let inputDiv1a = document.createElement('input');
+      inputDiv1a.setAttribute('type','text') + inputDiv1a.setAttribute('id','username') + inputDiv1a.setAttribute('placeholder','Username');
+      inputDiv1a.className = 'form-control';
+      inputDiv1a.setAttribute('required','')
+      div1a.appendChild(inputDiv1a);
+      let div1a2 = document.createElement('div');
+      div1a2.className = 'invalid-feedback';
+      div1a2.setAttribute('style','100%;');
+      div1a2Text = document.createTextNode('Your username is required.');
+      div1a2.appendChild(div1a2Text);
+      div1a.appendChild(div1a2);
+
+      div1.appendChild(div1a);
+
+      return div1;
+    }
+
+    form.appendChild(row1Form()) && form.appendChild(divsForm());
+
+    return form;
+  }
+  div.appendChild(form());
 
   return div;
 }
